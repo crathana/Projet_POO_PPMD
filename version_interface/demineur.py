33569540dea188ab7b923e4f91971605c2e16fbb
crathana <clement.rathana@gmail.com>
2025-11-12 14:46:54 +0100

    ajout de commentaires

--- version_interface/demineur.py
+++ version_interface/demineur.py
@@ -11,11 +11,9 @@ class DemineurGUI(QWidget):
     def __init__(self, difficulte="Facile"):
         super().__init__()
 
-        # Moteur du jeu
         self.grille = Grille(difficulte)
         self.premier_clic = True
 
-        # UI
         self.setWindowTitle(f"Démineur — {difficulte}")
         self.layout = QGridLayout()
         self.setLayout(self.layout)
@@ -25,6 +23,7 @@ class DemineurGUI(QWidget):
         self.difficulte = difficulte
 
     def creer_grille(self,difficulte):
+        """ Génère la grille en mettant en place une alternance de couleurs"""
         couleurs = ["#EEEED2", "#555A6A"]
 
         for i in range(self.grille.taille[0]):
@@ -45,7 +44,7 @@ class DemineurGUI(QWidget):
             self.boutons.append(ligne)
 
     def clic_case(self, event, x, y):
-
+        """ Gère l'action du clic sur une case, en adaptant si c'est le premier clic ou non """
         if self.premier_clic:
             self.grille.generer_grille((x, y))
             self.premier_clic = False
@@ -63,9 +62,11 @@ class DemineurGUI(QWidget):
             self.fin_partie()
 
     def action_reveler(self, x, y):
+        """ Révèle la case de coordonnées (x,y)"""
         self.grille.reveler(x, y)
 
     def action_drapeau(self, x, y):
+        """ Ajoute ou enleve un drapeau """
         case = self.grille.grille[x][y]
         if case.etat_case == 0:
             self.grille.cocher(x, y)
@@ -73,6 +74,7 @@ class DemineurGUI(QWidget):
             self.grille.decocher_case(x, y)
 
     def mettre_a_jour_affichage(self):
+        """ Mets à jour l'affichage des cases """
         for i in range(self.grille.taille[0]):
             for j in range(self.grille.taille[1]):
                 bouton = self.boutons[i][j]
